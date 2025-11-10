ee3d6973b5f8df81f780983e5a9e457131bc7b8c
rylena <rylen.anil@gmail.com>
2025-11-10 21:40:38 +0400

    fix: Final resolution for CardDisplay client component and default export issues

--- components/CardDisplay.tsx
+++ components/CardDisplay.tsx
@@ -1,4 +1,13 @@
+'use client'
+
 import Image from 'next/image'
 import { Card } from '@/types/database'
-import QRCodeComponent from './QRCode' // Ensure this is importing from ./QRCode
+import QRCodeComponent from './QRCode'
 import { useState } from 'react'
+
+interface CardDisplayProps {
+  card: Card
+  baseUrl: string
+}
+
+export default function CardDisplay({ card, baseUrl }: CardDisplayProps) {
